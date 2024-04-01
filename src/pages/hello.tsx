51175f48c0dee5e1ea1cbb700d25350eb44c8f60
sempitrnal <reynaldsampelo@gmail.com>
2024-04-02 06:18:35 +0800

    ref: easter egg

--- src/pages/hello.tsx
+++ src/pages/hello.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
@@ -18,7 +19,44 @@ function Hello() {
       }}
     >
       <Link href="/">Back</Link>
-      <h1 className="text-[10rem]">aiko hell yeah 👋🏽</h1>
+      <h1 className="text-[2rem]">aiko hell yeah 👋🏽</h1>
+      <div className="flex items-center gap-5 mt-24">
+        <video
+          className=""
+          autoPlay
+          loop
+          style={{ width: "", height: "500px" }}
+        >
+          <source src="/biko/biko.webm" />
+        </video>
+        <p className="">bo and aiko best couple 💗💗💗</p>
+      </div>
+      <div className="flex flex-col items-center gap-5 mt-16">
+        <p className="text-lg font-semibold">eepy bo & aiko</p>
+        <Image
+          className="rounded-md"
+          src="/biko/biko-sleep.jpeg"
+          width={250}
+          height={200}
+          alt="biko"
+        />
+      </div>
+      <div className="flex flex-col items-center gap-5 mt-16">
+        <p className="text-lg font-semibold">my pretty girl {"<3"}</p>
+        <Image
+          className="rounded-md"
+          src="/biko/aiko.jpeg"
+          width={250}
+          height={200}
+          alt="biko"
+        />
+      </div>
+      <div className="mt-5">
+        <hr />
+      </div>
+      <div className="flex flex-col items-center gap-5 mt-28">
+        <p className="font-mono">hi aiko, u are the best {"<3"}</p>
+      </div>
     </motion.div>
   );
 }
